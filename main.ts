1b2e06331aa9dcfe2ab2ac45b0f3c6e5d423020d
hdenk <68135+hdenk@users.noreply.github.com>
2022-01-07 06:04:06 +0100

    Initial Commit

--- main.ts
+++ main.ts
@@ -1,22 +1,121 @@
 function saugerNachRechts () {
-	
+    if (saugerRaum == 1) {
+        saugerRaum = 2
+    }
 }
 function raumStaubigMachen () {
-	
+    if (randint(1, 2) == 1) {
+        staubInRaum1 = true
+    } else {
+        staubInRaum2 = true
+    }
 }
+input.onButtonPressed(Button.A, function () {
+    saugerNachLinks()
+})
 function saugerNachLinks () {
-	
+    if (saugerRaum == 2) {
+        saugerRaum = 1
+    }
+}
+input.onButtonPressed(Button.B, function () {
+    saugerNachRechts()
+})
+input.onGesture(Gesture.Shake, function () {
+    raumSaugen()
+})
+function createDisplayStates () {
+    return [
+    images.createImage(`
+        # # # . .
+        # # # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `),
+    images.createImage(`
+        . . # # #
+        . . # # #
+        . . # . .
+        . . # . .
+        . . # . .
+        `),
+    images.createImage(`
+        # # # . .
+        # # # . .
+        . . # . .
+        . . # . .
+        # # # . .
+        `),
+    images.createImage(`
+        . . # # #
+        . . # # #
+        . . # . .
+        . . # . .
+        # # # . .
+        `),
+    images.createImage(`
+        # # # . .
+        # # # . .
+        . . # . .
+        . . # . .
+        . . # # #
+        `),
+    images.createImage(`
+        . . # # #
+        . . # # #
+        . . # . .
+        . . # . .
+        . . # # #
+        `),
+    images.createImage(`
+        # # # . .
+        # # # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `),
+    images.createImage(`
+        . . # # #
+        . . # # #
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    ]
 }
 function raumSaugen () {
-	
+    if (saugerRaum == 1) {
+        staubInRaum1 = false
+    } else {
+        staubInRaum2 = false
+    }
 }
 function staubsaugerWeltVisualisieren (pSaugerRaum: number, staubInRaum1: boolean, staubInRaum2: boolean) {
-	
+    displayStateAsNumber = pSaugerRaum - 1
+    if (staubInRaum1) {
+        displayStateAsNumber = displayStateAsNumber + 2
+    }
+    if (staubInRaum2) {
+        displayStateAsNumber = displayStateAsNumber + 4
+    }
+    currentDisplay = displayStates[displayStateAsNumber]
+    currentDisplay.showImage(0)
 }
-let saugerRaum = 1
+let currentDisplay: Image = null
+let displayStateAsNumber = 0
+let displayStates: Image[] = []
+let staubInRaum2 = false
 let staubInRaum1 = false
-let staubInRaum2 = true
-let displayStates = 0
+let saugerRaum = 0
+saugerRaum = 1
+staubInRaum1 = false
+staubInRaum2 = true
+displayStates = createDisplayStates()
 basic.forever(function () {
-	
+    if (randint(0, 1000) == 13) {
+        raumStaubigMachen()
+    }
+    staubsaugerWeltVisualisieren(saugerRaum, staubInRaum1, staubInRaum2)
+    basic.pause(10)
 })

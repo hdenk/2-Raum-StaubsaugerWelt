850301fae167136204cae408f418812250f68746
hdenk <68135+hdenk@users.noreply.github.com>
2022-01-07 05:23:44 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+function saugerNachRechts () {
+	
+}
+function raumStaubigMachen () {
+	
+}
+function saugerNachLinks () {
+	
+}
+function raumSaugen () {
+	
+}
+function staubsaugerWeltVisualisieren (pSaugerRaum: number, staubInRaum1: boolean, staubInRaum2: boolean) {
+	
+}
+let saugerRaum = 1
+let staubInRaum1 = false
+let staubInRaum2 = true
+let displayStates = 0
+basic.forever(function () {
+	
+})
